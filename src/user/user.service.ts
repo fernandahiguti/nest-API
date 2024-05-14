95260bab0d215a79c515ed465f0fbcabd90fcf77
Fernanda Higuti <fernandamh1996@gmail.com>
2024-05-14 11:45:37 -0300

    does not allow more than one user with the same email

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,10 +7,18 @@ import { CreateUserDto } from './dto/create-user.dto';
 @Injectable()
 export class UserService {
   constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
-  
+
   async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const existingUser = await this.findUserByWorkEmail(createUserDto.workEmail);
+    if (existingUser) {
+      throw new Error('There is already an account with this email');
+    }
     const createdUser = new this.userModel(createUserDto);
     return createdUser.save();
+  }
+
+  async findUserByWorkEmail(workEmail: string): Promise<User | null> {
+    return this.userModel.findOne({ workEmail }).exec();
   }
 
 }
